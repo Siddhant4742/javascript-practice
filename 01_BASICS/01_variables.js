c29a72b7b92958566ea2beab7ff6c313a692d864
Sidhant Andure <siddhantandure@gmail.com>
2025-05-24 06:37:06 +0000

    variables files created

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=12345 // cannot be changes
+let accountName="Siddhant" // can be changed
+var accountEmail="sidhant@gmail.com" //can be changes but has scope problems 
+let accountState;
+accountCity="Pune" //can be changed
+
+console.log(accountId);//print one varible
+console.table([accountId,accountName,accountEmail,accountCity,accountState])//print in form of a table
