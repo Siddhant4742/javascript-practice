1cc58fee00bcbcbacd7a9fe899a735430e4aa956
Sidhant Andure <siddhantandure@gmail.com>
2025-05-24 06:50:28 +0000

    dataypes added

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,10 @@
+let name="siddhant" //string
+let age=20// number
+let isLoggedIn=true//boolean
+let state;//undefined
+let data=null //object
+
+//symbol=> unique
+
+console.log(typeof state);
+console.log(typeof null);
